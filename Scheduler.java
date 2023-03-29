2c827cc898bb2816b3487596b9a9e8a40a703c45
iacobus312 <111306468+iacobus312@users.noreply.github.com>
2023-03-29 09:08:05 -0400

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -6,14 +6,16 @@ public class Scheduler {
 
         String string_num = input.nextLine(); // number of shifts needed
         int num = Integer.parseInt(string_num);
-
+        int shift_counter = 0;
         String[] nurses = new String[num]; // to store input data about nurses
+        String[] shifts = new String[num]; // to track shifts
 
         for (int i = 0; i < num; i++) {
-            nurses[i] = input.nextLine().replaceAll("\\s", "") + input.nextLine().replaceAll("\\s", "");
+            nurses[i] = input.nextLine() + input.nextLine();
+            nurses[i] = nurses[i].replaceAll("\\s", "");
+            shifts[i] = Character.toString(nurses[i].charAt(0));
             nurses[i] = nurses[i].substring(2);
         }
-
 
         /*
         for (String i : nurses) {
@@ -36,23 +38,36 @@ public class Scheduler {
 
         Indexes of i are already sorted per nurse, aka i[0] = A, i[1] = B, etc. Numbers dicate which shifts
         the nurses can fulfill
+        nurses will just keep track of the names
          */
-        solve(num, nurses);
+        for (String shiftnum : shifts) {
+            System.out.println(shiftnum);
+        }
+        solve(num, shift_counter, nurses, shifts);
     }
 
-    public static String solve(int a, String[] i) {
-        return "";
+    public static String solve(int num_shifts, int shiftc, String[] shifts_requested, String[] schedule) {
+        /*
+        shifts_requested:
+        1
+        12
+        02
+
+        schedule:
+        A
+        B
+        C
+         */
+        if (shiftc == num_shifts) {
+            return "success!";
+        } else {
+            return "impossible!";
+        }
+        // pseudocode, 
+        // run at first with shiftc = 0
+        // run recursively until shiftc = 1
+        // basically for every possible nurse that can have a certain shift, give her that shift, and then run with shiftc+1
+        // conditions is that nurse can have that shift and cannot already be placed into a shift
+        // find a way to connect the recursive loop together
     }
 }
-
-/*
-solve(Nurses)
-  Check if all nurses are assigned a shift, if so, then
-    return success
-  Let N be a nurse without a shift
-  for each shift s
-    if N can do s, and no one else is assigned s, then
-      Assign N to s
-      if solve(Nurses) is success then return success
-  return "impossible"
- */
